c24391b987f6707ffeaf230d75f1d183e9fae1d5
santa137 <34958687+santa137@users.noreply.github.com>
2018-01-16 19:33:33 +0300

    Add files via upload

---
+++ 4/SingletonPattern/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton single = Singleton.GetInstance();
+            single.flag = true;
+            Console.WriteLine(single.flag);
+
+            Singleton single2 = Singleton.GetInstance();
+            single2.flag = false;
+            Console.WriteLine(single.flag);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 4/SingletonPattern/Singleton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    // Класс должен быть запечатаннным (модификатор sealed).
+    // Запрет создания наследников.
+    sealed class Singleton
+    {  
+
+        // Закроем конструктор для доступа из вне.
+        private Singleton() { }
+
+        // Один экземпляр класса, который будет создаваться при первом обращении к классу.
+        private static Singleton single = new Singleton();
+
+        // Метод, который будет возвращать ссылку.
+        public static Singleton GetInstance()
+        {
+            return single;
+        }
+
+        public bool flag;
+
+    }
+}
